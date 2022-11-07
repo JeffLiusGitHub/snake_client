61475232eebdaafff920752fcb8218045cde1f98
Jeff Liu <49432090+JeffLiusGitHub@users.noreply.github.com>
2022-11-08 08:20:23 +0930

    change append logic create useAppend hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import React, { useState, useEffect } from 'react';
 import Header from './Component/Header';
 import Card from './Component/Card';
 import Layout from './Component/Layout';
-import { IncomingDataType, snakeBidType } from './type';
+import { snakeBidType } from './type';
 import { IMessageEvent, w3cwebsocket } from 'websocket';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './Component/ErrorBoundary';
-
+import Append from './Component/Helper/useAppend';
 function App() {
 	const name = 'Jeff';
 
+	const { websocketData, setIncomingData } = Append();
 	const [error, setError] = useState<boolean>(false);
-	const [incomingData, setIncomingData] = useState<IncomingDataType>();
-	const [websocketData, setWebsocketData] = useState<snakeBidType[]>([]);
 
 	const BASE_URL: string = 'ws://localhost:8080/api/updates';
 
@@ -33,85 +32,7 @@ function App() {
 		};
 
 		return () => client.close();
-	}, []);
-
-	useEffect(() => {
-		const appendData = (): void => {
-			if (websocketData.length === 0) {
-				if (incomingData) {
-					const { id, stage, bid }: IncomingDataType = incomingData;
-					return setWebsocketData([
-						{ id: id, stage: stage, highest: bid, TVL: bid, bidArray: [bid!] },
-					]);
-				}
-			} else {
-				const insertDataIndex: number = websocketData.findIndex(
-					(w) => w.id === incomingData?.id
-				);
-				if (insertDataIndex === -1) {
-					const { id, stage, bid }: IncomingDataType = incomingData!;
-					return setWebsocketData((prevData) => [
-						...prevData,
-						{ id: id, stage: stage, highest: bid, TVL: bid, bidArray: [bid!] },
-					]);
-				} else {
-					const existingSnake: snakeBidType = websocketData[insertDataIndex];
-					const { id, highest, TVL, bidArray }: snakeBidType = existingSnake!;
-					const { bid, stage }: IncomingDataType = incomingData!;
-					let newHighest: number;
-
-					switch (incomingData?.stage) {
-						case 1:
-							if (highest! > incomingData?.bid!) {
-								newHighest = highest;
-							} else {
-								newHighest = incomingData.bid;
-							}
-							websocketData[insertDataIndex] = {
-								id: id,
-								stage: stage,
-								highest: newHighest,
-								TVL: TVL + bid,
-								bidArray: [...bidArray, bid],
-							};
-							return setWebsocketData(websocketData);
-
-						case 2:
-							if (highest! > incomingData?.bid!) {
-								newHighest = highest;
-							} else {
-								newHighest = incomingData.bid;
-							}
-							websocketData[insertDataIndex] = {
-								id: id,
-								stage: stage,
-								highest: newHighest,
-								TVL: TVL - bid,
-								bidArray: [...bidArray, bid],
-							};
-							return setWebsocketData(websocketData);
-
-						case 3:
-							if (highest! > incomingData?.bid!) {
-								newHighest = highest;
-							} else {
-								newHighest = incomingData.bid;
-							}
-							websocketData[insertDataIndex] = {
-								id: id,
-								stage: stage,
-								highest: newHighest,
-								TVL: 0,
-								bidArray: [...bidArray],
-							};
-							return setWebsocketData(websocketData);
-					}
-				}
-			}
-		};
-		appendData();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [incomingData]);
+	}, [BASE_URL, setIncomingData]);
 
 	return (
 		<ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => {}}>
