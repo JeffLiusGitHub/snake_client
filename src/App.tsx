5adc332d80b929241c8a6cdd6ea5aa6a37aa4e10
Jeff Liu <49432090+JeffLiusGitHub@users.noreply.github.com>
2022-11-09 09:51:49 +0930

    fix double render, change setState to use PreviousState version

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,93 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Header from './Component/Header';
 import Card from './Component/Card';
 import Layout from './Component/Layout';
-import { snakeBidType } from './type';
+import { snakeBidType, IncomingDataType } from './type';
 import { IMessageEvent, w3cwebsocket } from 'websocket';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './Component/ErrorBoundary';
-import useAppend from './Component/Helper/useAppend';
+
 function App() {
 	const name = 'Jeff';
 
-	const { websocketData, setIncomingData } = useAppend();
+	const [websocketData, setWebsocketData] = useState<snakeBidType[]>([]);
+
 	const [error, setError] = useState<boolean>(false);
 
 	const BASE_URL: string = 'ws://localhost:8080/api/updates';
 
+	const appendData = useCallback((incomingData: IncomingDataType): void => {
+		setWebsocketData((prevData: snakeBidType[]): snakeBidType[] => {
+			if (prevData?.length === 0) {
+				if (incomingData) {
+					const { id, stage, bid }: IncomingDataType = incomingData;
+					return [
+						{
+							id: id,
+							stage: stage,
+							highest: bid,
+							TVL: bid,
+							bidArray: [bid!],
+						},
+					];
+				}
+			} else {
+				const insertDataIndex: number = prevData?.findIndex(
+					(w: any) => w.id === incomingData?.id
+				);
+				if (insertDataIndex === -1) {
+					const { id, stage, bid }: IncomingDataType = incomingData!;
+					return [
+						...prevData,
+						{
+							id: id,
+							stage: stage,
+							highest: bid,
+							TVL: bid,
+							bidArray: [bid!],
+						},
+					];
+				} else {
+					const existingSnake: snakeBidType = prevData[insertDataIndex];
+					const { id, highest, TVL, bidArray }: snakeBidType = existingSnake!;
+					const { bid, stage }: IncomingDataType = incomingData!;
+
+					switch (incomingData?.stage) {
+						case 1:
+							prevData[insertDataIndex] = {
+								id: id,
+								stage: stage,
+								highest: Math.max(highest, incomingData?.bid!),
+								TVL: TVL + bid,
+								bidArray: [...bidArray, bid],
+							};
+							return [...prevData];
+
+						case 2:
+							prevData[insertDataIndex] = {
+								id: id,
+								stage: stage,
+								highest: Math.max(highest, incomingData?.bid!),
+								TVL: TVL - bid,
+								bidArray: [...bidArray, bid],
+							};
+							return [...prevData];
+
+						case 3:
+							prevData[insertDataIndex] = {
+								id: id,
+								stage: stage,
+								highest: Math.max(highest, incomingData?.bid!),
+								TVL: 0,
+								bidArray: [...bidArray],
+							};
+							return [...prevData];
+					}
+				}
+			}
+			return prevData;
+		});
+	}, []);
 	useEffect(() => {
 		const client = new w3cwebsocket(BASE_URL);
 		client.onopen = (): void => {
@@ -24,7 +97,7 @@ function App() {
 
 		client.onmessage = (message: IMessageEvent): void => {
 			const dataFromServer = JSON.parse(message?.data.toString());
-			setIncomingData(dataFromServer);
+			appendData(dataFromServer);
 		};
 
 		client.onerror = (): void => {
@@ -32,7 +105,7 @@ function App() {
 		};
 
 		return () => client.close();
-	}, [BASE_URL, setIncomingData]);
+	}, [BASE_URL, appendData]);
 
 	return (
 		<ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => {}}>
